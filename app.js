9985cb2ee5471e8499cf6c4963665f05e86703e7
Kyle Gonzales <gonzales.j.kyle@gmail.com>
2024-02-26 17:30:39 -0800

    Added mongodb queries, data processing parameters, and error handling when microservice is not running

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const button = document.getElementById('microservice-btn');
 // Add event listener to the button
 button.addEventListener('click', () => {
     const ws = new WebSocket("ws://127.0.0.1:65432/")
-    heading.textContent = "Server not running.";
 
+    // Check if the microservice is running
+    ws.addEventListener("error", (event) => {
+        heading.textContent = "Server not running.";
+    });
+
+    // No errors, retreive data and update web page
     ws.onmessage = function(event){
-        heading.textContent = event.data;
-        ws.close();
-    };
-    
+    heading.textContent = event.data;
+    ws.close();
+    }
 });
--- microservice.py
+++ microservice.py
@@ -1,25 +1,81 @@
 import asyncio
 import websockets
 import json
+import pymongo
 
 
 async def data(websocket):
-    test_data = {"Player": "Bob Ross",
-            "Passing Yards": 183,
-            "Receptions": 12,
-            "Receiving Yards": 87,
-            "Rushing Yards": 43,
-            "Fumbles": 2,
-            "Interceptions": 0
-            }
+    """
+    Sends JSON data to the client
+    :param websocket
+    """
+    client = db_connect()
+    final_score = process_data(client)
 
-    return_data = json.dumps(test_data)
+    send_data = {"Final Score": final_score}
+
+    return_data = json.dumps(send_data)
     await websocket.send(return_data)
 
 
 async def main():
+    """
+    Listens for incoming connections at
+    HOST: 127.0.0.1
+    PORT: 65432
+    """
     async with websockets.serve(data, "127.0.0.1", 65432):
         await asyncio.Future()
+
+
+def db_connect() -> pymongo:
+    """
+    Connect to mongodb from python using pymongo
+    :return: pymongo client object
+    """
+    # Insert credentials here XXXX
+    return pymongo.MongoClient("XXXX")
+
+
+def process_data(client: pymongo) -> int:
+    """
+    Process data from mongodb to find total team score
+    :param client: pymongo client object
+    :return: int
+    """
+    # Open the DB
+    dbname = client['test']
+
+    # Get players collection
+    players = dbname["players"]
+
+    # Parse collection
+    entries_list = []
+    for entry in players.find():
+        entry_list = [entry['passingYards'], entry['receptions'], entry['receivingYards'], entry['rushingYards'], entry['fumbles'], entry['interceptions']]
+        entries_list.append(entry_list)
+
+    summation = [0, 0, 0, 0, 0, 0]
+
+    # Calculate the sum for each statistic
+    for player in entries_list:
+        for stat in range(0,len(player)):
+            summation[stat] = summation[stat] + player[stat]
+
+    scoring_values = dbname["stats"]
+
+    # Obtain value for each statistic and add to the stat_list
+    stat_list = None
+    for statistic in scoring_values.find():
+        stat_list = [statistic['passingYards'], statistic['receptions'], statistic['receivingYards'], statistic['rushingYards'], statistic['fumbles'], statistic['interceptions']]
+
+    # Calculate point value for each statistic
+    total_score = []
+    for i in range(0, len(stat_list)):
+        total_score.append((stat_list[i] * summation[i]))
+
+    print(sum(total_score))
+    return sum(total_score)
 
 
 if __name__ == "__main__":
